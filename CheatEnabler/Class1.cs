207d9a8d89d488199c11358372665dcef66fe66b
Spycrab.gg <64327213+Spycrab2@users.noreply.github.com>
2024-03-21 22:21:48 -0400

    Add files via upload

---
+++ CheatEnabler/Class1.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using UnityEngine;
+
+[BepInPlugin("com.Spycrab.enablecheatsmod", "Cheats Mod", "1.0.0")]
+public class CheatsMod : BaseUnityPlugin
+{
+    void Update()
+    {
+        // LOLOLOLOLOLOLO IM FUCKING GOING INSANEEEEEE 
+        CheatScript cheating = FindObjectOfType<CheatScript>();
+
+        // This took 2 min
+        if (cheating != null && !cheating.enabled)
+        {
+            cheating.enabled = true;
+        }
+        else if (cheating == null)
+        {
+            Debug.Log("Cheat script is not enabled bozo");
+        }
+        else
+        {
+            Debug.Log("chatscript is installed not easy");
+        }
+    }
+}
